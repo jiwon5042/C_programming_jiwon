a4d1146912d60a643a44e6f07fb4b46f8b394c9e
jiwon5042 <namjiwon5042@gmail.com>
2026-03-13 05:47:30 +0000

    6th

---
+++ lec1/lec1_6th.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 3;
+    ++a;
+    printf("a: %d\n",a);
+    printf("a: %d\n",a++);
+    printf("a: %d\n",a);
+
+    a++;
+    printf("a: %d\n",a);
+    printf("a: %d\n",a++);
+    printf("a: %d\n",a);
+    return 0;
+}
