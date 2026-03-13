9b234fd1e4316ef8215833c99e11a738975c4fae
jiwon5042 <namjiwon5042@gmail.com>
2026-03-13 05:38:50 +0000

    study

---
+++ lec1/lec1_1st.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+char a = 'A';
+ printf("a %%d: %d\n", a);
+ printf("a %%i: %i\n", a);
+ printf("a %%c: %c\n", a+32);
+ printf("a %%f: %f\n", (float)a);
+ printf("-----\n"); 
+    return 0;
+}
---
+++ lec1/lec1_2nd.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char b[5] = "ab";
+    printf("b %%c: %c\n", b);
+    printf("b %%d: %d\n", b);
+    printf("b %%s: %s\n", b);
+    return 0;
+}
